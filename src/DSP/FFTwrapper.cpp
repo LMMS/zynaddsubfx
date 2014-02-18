0a4010b5f4483e629a081ed08c57de0f2f5535f3
Tobias Doerffel <tobias.doerffel@gmail.com>
2014-02-18 19:04:39 +0100

    FFTwrapper: do FFT with single precision
    LMMS ships with single precision version of fftw library. In order to not
    having to ship another variant, change calculations to single precision here.
    We still have to check wether this has an impact on sound generation.

--- src/DSP/FFTwrapper.cpp
+++ src/DSP/FFTwrapper.cpp
@@ -29,12 +29,12 @@ FFTwrapper::FFTwrapper(int fftsize_)
 {
     fftsize  = fftsize_;
     time     = new fftw_real[fftsize];
-    fft      = new fftw_complex[fftsize + 1];
-    planfftw = fftw_plan_dft_r2c_1d(fftsize,
+    fft      = new fftwf_complex[fftsize + 1];
+    planfftw = fftwf_plan_dft_r2c_1d(fftsize,
                                     time,
                                     fft,
                                     FFTW_ESTIMATE);
-    planfftw_inv = fftw_plan_dft_c2r_1d(fftsize,
+    planfftw_inv = fftwf_plan_dft_c2r_1d(fftsize,
                                         fft,
                                         time,
                                         FFTW_ESTIMATE);
@@ -42,8 +42,8 @@ FFTwrapper::FFTwrapper(int fftsize_)
 
 FFTwrapper::~FFTwrapper()
 {
-    fftw_destroy_plan(planfftw);
-    fftw_destroy_plan(planfftw_inv);
+    fftwf_destroy_plan(planfftw);
+    fftwf_destroy_plan(planfftw_inv);
 
     delete [] time;
     delete [] fft;
@@ -53,26 +53,26 @@ void FFTwrapper::smps2freqs(const float *smps, fft_t *freqs)
 {
     //Load data
     for(int i = 0; i < fftsize; ++i)
-        time[i] = static_cast<double>(smps[i]);
+        time[i] = smps[i];
 
     //DFT
-    fftw_execute(planfftw);
+    fftwf_execute(planfftw);
 
     //Grab data
-    memcpy((void *)freqs, (const void *)fft, fftsize * sizeof(double));
+    memcpy((void *)freqs, (const void *)fft, fftsize * sizeof(fftw_real));
 }
 
 void FFTwrapper::freqs2smps(const fft_t *freqs, float *smps)
 {
     //Load data
-    memcpy((void *)fft, (const void *)freqs, fftsize * sizeof(double));
+    memcpy((void *)fft, (const void *)freqs, fftsize * sizeof(fftw_real));
 
     //clear unused freq channel
     fft[fftsize / 2][0] = 0.0f;
     fft[fftsize / 2][1] = 0.0f;
 
     //IDFT
-    fftw_execute(planfftw_inv);
+    fftwf_execute(planfftw_inv);
 
     //Grab data
     for(int i = 0; i < fftsize; ++i)
@@ -81,5 +81,5 @@ void FFTwrapper::freqs2smps(const fft_t *freqs, float *smps)
 
 void FFT_cleanup()
 {
-    fftw_cleanup();
+    fftwf_cleanup();
 }
