ddd3bd4f782f1655bf8a11de45cbead30dcecc4a
Hans Petter Selasky <hps@selasky.org>
2014-10-24 11:01:48 +0200

    Fix C++ standards library portability issue.
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>

--- src/Params/PADnoteParameters.cpp
+++ src/Params/PADnoteParameters.cpp
@@ -614,8 +614,10 @@ void PADnoteParameters::applyparameters(bool lockmutex)
 
         newsample.smp[0] = 0.0f;
         for(int i = 1; i < spectrumsize; ++i) //randomize the phases
-            fftfreqs[i] = std::polar(spectrum[i], (float)RND * 6.29f);
-        fft->freqs2smps(fftfreqs, newsample.smp); //that's all; here is the only ifft for the whole sample; no windows are used ;-)
+            fftfreqs[i] = FFTpolar(spectrum[i], (float)RND * 2 * PI);
+        //that's all; here is the only ifft for the whole sample;
+        //no windows are used ;-)
+        fft->freqs2smps(fftfreqs, newsample.smp);
 
 
         //normalize(rms)
